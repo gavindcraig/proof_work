d2bdb1a1902cda8291f9729fdad0522bbbe5f263
Gavin D. Craig <67200393+gavindcraig@users.noreply.github.com>
2021-12-23 21:53:02 -0600

    Add variable difficulty

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## Goal
 Create a game to find proof of work.
 
+## Usage
+`$ python3 proof_work.py {complexity}`
+
+`{complexity}` is the number of zeroes that must be present at the end of the hash in order for the proof of work to succeed.
+
--- proof_work.py
+++ proof_work.py
@@ -2,6 +2,7 @@ import numpy
 import hashlib
 import string
 import itertools
+import sys
 
 
 class Block:
@@ -21,14 +22,23 @@ class Block:
 
 
 def main():
-    b = Block()
+    if len(sys.argv) > 1:
+        a = int(sys.argv.pop())
+    else:
+        a = 1
+    b = Block(tail=a)
     i = 1
     while True:
-        for c in itertools.permutations(string.ascii_letters + string.digits,
-                                        i):
-            if b.guess(''.join(c)):
-                print('Proof of work: ' + ''.join(c))
-                exit(0)
+        try:
+            for c in itertools.permutations(string.ascii_letters + string.digits,
+                                            i):
+                if b.guess(''.join(c)):
+                    print('Proof of work: ' + ''.join(c))
+                    exit(0)
+        except KeyboardInterrupt:
+            print('Cancelled\n' +
+                  'Last proof attempted: ' + ''.join(c))
+            exit(130)
         i = i + 1
 
 
